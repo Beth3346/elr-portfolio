2d03ee3c32bfc0fe27f0b280457656007122ef26
Elizabeth Rogers <beth3346@gmail.com>
2017-03-14 16:40:10 -0500

    styled post box

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
-"use strict";
+'use strict';
+
+(function ($) {
+    'use strict';
+
+    var gotoSection = function gotoSection(e) {
+        e.preventDefault();
+
+        var $that = $(this);
+        var target = $that.attr('href');
+        var $content = $('body, html');
+        var $target = $(target);
+
+        console.log(target);
+
+        $content.stop().animate({
+            'scrollTop': $target.position().top - 50
+        });
+    };
+
+    $('.smooth-scroll a').on('click', gotoSection);
+})(jQuery);
