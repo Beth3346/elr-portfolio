1a5c9c6f13e2d1be454dd2a38a15fcc18011c019
Elizabeth Rogers <beth3346@gmail.com>
2017-05-27 11:45:44 -0500

    add author information wrapper to author shortcode

--- framework/shortcodes/Authors.php
+++ framework/shortcodes/Authors.php
@@ -7,12 +7,14 @@ class Authors
     {
         $string = '<div class="author-box">';
             $string .= '<h3 class="author-title">Written by: ' . get_the_author() . '</h3>';
-            $string .= '<div class="author-avatar">';
-                $string .= get_avatar( get_the_author_meta('user_email'), '80', '' );
-            $string .= '</div>';
-            $string .= '<div class="author-description">';
-                $string .= wpautop(get_the_author_meta('description'));
-                $string .= '<p><a href="' . get_author_posts_url(get_the_author_meta( 'ID' )) . '">Read more from ' . get_the_author() . '</a></p>';
+            $string .= '<div class="author-info">';
+                $string .= '<div class="author-avatar">';
+                    $string .= get_avatar( get_the_author_meta('user_email'), '80', '' );
+                $string .= '</div>';
+                $string .= '<div class="author-description">';
+                    $string .= wpautop(get_the_author_meta('description'));
+                    $string .= '<p><a href="' . get_author_posts_url(get_the_author_meta( 'ID' )) . '">Read more from ' . get_the_author() . '</a></p>';
+                $string .= '</div>';
             $string .= '</div>';
         $string .= '</div>';
 
