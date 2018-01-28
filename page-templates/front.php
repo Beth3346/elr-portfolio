288ce89b2d81630402919a5b4cbfaa950423b327
Elizabeth Rogers <beth3346@gmail.com>
2018-01-27 19:58:48 -0600

    clear up front page template

--- page-templates/front.php
+++ page-templates/front.php
@@ -7,13 +7,16 @@ $context = Timber::get_context();
 $context['post'] = $post;
 $context['articles'] = Timber::get_posts('post_type=post&numberposts=3');
 $context['educations'] = Timber::get_posts('post_type=education&numberposts=3');
-$context['experiences'] = Timber::get_posts('post_type=experience&numberposts=3');
-$context['projects'] = Timber::get_posts('post_type=project&numberposts=3');
+$context['experiences'] = Timber::get_posts('post_type=experience&numberposts=-1');
+$context['websites'] = Timber::get_posts('post_type=project&numberposts=3');
 $context['readings'] = Timber::get_posts('post_type=reading&numberposts=3');
 $context['recommendations'] = Timber::get_posts('post_type=recommendation&numberposts=3');
 $context['skills'] = Timber::get_posts('post_type=skill&numberposts=-1');
 $context['tutorials'] = Timber::get_posts('post_type=tutorial&numberposts=3');
 $context['videos'] = Timber::get_posts('post_type=video&numberposts=3');
 $context['summary'] = wpautop(get_option('general_options')['summary']);
+
+// open source projects
+// $context['projects'] = Timber::get_posts('post_type=project&numberposts=3');
 
 Timber::render('front-page.twig', $context);
